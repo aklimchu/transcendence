8d17bdcf6efca25dffb0d10e798a4f5c4b35abc9
Lucas Opoka <lopoka@student.hive.fi>
2025-02-10 17:36:03 +0200

    Started extending standard Django user model with PongSession.

--- backend/backend_source/pong/models.py
+++ backend/backend_source/pong/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
+
+class PongSession(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    text_field = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.user.username
+
+# On signal create PongSession for User
+@receiver(post_save, sender=User)
+def create_pong_session(sender, instance, created, **kwargs):
+    if created:
+        PongSession.objects.create(user=instance)
+    instance.pongsession.save()
+
+# On signal delete user connected to PongSession
+@receiver(post_delete, sender=PongSession)
+def delete_pong_session(sender, instance, **kwargs):
+    if instance.user:
+        instance.user.delete()
