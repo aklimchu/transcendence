fd6963a7a292bce69071b830673f2da5c149d4af
atormac <atorma@student.hive.fi>
2025-02-24 15:20:33 +0200

    Basic stats fetch

--- frontend/frontend_source/js/views/StatsView.js
+++ frontend/frontend_source/js/views/StatsView.js
@@ -7,14 +7,36 @@ export default class extends AbstractView
 	super(params);
     }
 
+    async fetchStatsData()
+    {
+        try
+        {
+            var response = await fetch("pong_api/pong_stats_data/", {method: "GET"});
+            if (!response.ok)
+                throw new Error("Failed to fetch statistics data");
+            return await response.json();
+        }
+        catch(err)
+        {
+            console.error(err.message);
+            if (response.status === 401)
+                this.goToNoAuth();
+            else
+                this.goToError();
+            throw err;
+        }
+    }
+
     async goToView()
     {
-	try {var json = await this.fetchSessionData();}
+	try {var json = await this.fetchStatsData();}
 	catch (err) {return;}
 
-	var content = `
+	console.log(json)
 
+	var content = `
 	<p> Statistics View </p>
+	<p>Total games played: ${json.data["total_games"]}</p>
 	`;
 
 	this.setTitle("Statistics");
