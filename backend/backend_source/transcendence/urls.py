3b8d897a61079375c0f34de44e8f79fdff5ce30a
aklimchu <aklimchu@student.hive.fi>
2025-06-27 12:04:38 +0300

    avatar are showed correctly in Settings page

--- backend/backend_source/transcendence/urls.py
+++ backend/backend_source/transcendence/urls.py
@@ -23,8 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pong_api/', include("pong.urls")),
-	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-	path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-#static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
